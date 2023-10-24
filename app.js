5f39a7a5de76901a37a4eb67b5879e45d66ff7f0
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-24 21:14:22 +0200

    done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // Pika-pika suerte!
+
+let backgroundPikachu = document.querySelector("#Pikachu");
+let buttonColor = document.querySelector("#colors");
+
+buttonColor.addEventListener("click", function (event){
+    console.log("el color pulsado es el: " + event.target.id);
+
+    backgroundPikachu.style.backgroundColor = event.target.id;
+
+});
